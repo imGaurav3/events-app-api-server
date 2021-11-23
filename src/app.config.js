d701510ca0e111efd7125dc57582a7438d6c4095
Ashish <ashish.g308@gmail.com>
2021-11-23 05:27:48 +0000

    Updated team's value

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    version = '1.0.1';
-    team = 'Your Team Name';
+    version = '1.0.2';
+    team = 'Team 4';
 }
 module.exports = AppConfig;
